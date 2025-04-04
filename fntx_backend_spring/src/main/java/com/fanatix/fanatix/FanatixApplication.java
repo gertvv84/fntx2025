15e3a1652e6629997f386e6d2f6efe2b00f0e5b7
gertvv84 <gert.van.vaerenbergh@gmail.com>
2025-04-04 10:30:58 +0200

    second commit

---
+++ fntx_backend_spring/src/main/java/com/fanatix/fanatix/FanatixApplication.java
@@ -0,0 +1,13 @@
+package com.fanatix.fanatix;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FanatixApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FanatixApplication.class, args);
+	}
+
+}
---
+++ fntx_backend_spring/src/test/java/com/fanatix/fanatix/FanatixApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fanatix.fanatix;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FanatixApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
